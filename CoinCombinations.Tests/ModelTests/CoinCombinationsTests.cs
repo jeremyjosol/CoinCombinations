b5f3663703a6438459b3dec0edd2852d263ab8bd
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-19 14:53:57 -0700

    Create new test for CalcChange which loops through coinsArray and creates list of change type + pass test
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Richard Barbour <richyb07@gmail.com>

--- CoinCombinations.Tests/ModelTests/CoinCombinationsTests.cs
+++ CoinCombinations.Tests/ModelTests/CoinCombinationsTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoinCombinations.Models;
 using System;
+using System.Collections.Generic;
 
 namespace CoinCombinations.Tests
 {
@@ -14,7 +15,26 @@ namespace CoinCombinations.Tests
     Coins coins = new Coins(userInput);
     int value = coins.Change;
     Assert.AreEqual(userInput, value);
+  }
 
-    }
+  [TestMethod]
+  public void CalcChange_SetsValueOfChange_Int()
+  {
+    Coins coins = new Coins(67);
+    int newChangeNumber = 34;
+    coins.Change = newChangeNumber;
+    Assert.AreEqual(newChangeNumber, coins.Change);
+
+  }
+
+  [TestMethod]
+  public void CalcChange_CreatesListOfChangeType_Void()
+  {
+    Coins coins = new Coins(67);
+    coins.CalcChange();
+    List<int> expected = new List<int> { 2, 1, 1, 2 };
+    CollectionAssert.AreEqual(expected, coins._results);
+  }
   }
 }
+
--- CoinCombinations/Models/CoinCombinations.cs
+++ CoinCombinations/Models/CoinCombinations.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace CoinCombinations.Models
 {
   public class Coins
   {
     public static int[] coinsArray = new int[] { 25, 10, 5, 1 };
+
+    public List<int> _results = new List<int> {}; 
 
     public int Change { get; set; }
 
@@ -13,10 +16,18 @@ namespace CoinCombinations.Models
       Change = change;
     }
 
-    // public void CalcChange(int change)
-    // {
-    //    Change = change;
-    // }
+    public void CalcChange()
+    {
+      int removedChange;
+      int result;
+
+      for (int i = 0; i < coinsArray.Length; i++)
+      {
+        result = Change / coinsArray[i];
+        _results.Add(result);
+        removedChange = coinsArray[i] * result;
+        Change = Change - removedChange;
+      }
+    }
   }
 }
-
