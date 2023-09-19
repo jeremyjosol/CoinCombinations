148d0d48cb9b1f25d0846daf37c8f571bb21dea1
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-19 09:36:07 -0700

    Initialize project + .gitignore

---
+++ CoinCombinations.Tests/ModelTests/CoinCombinationsTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+using System;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    // Test methods go here
+  }
+}
