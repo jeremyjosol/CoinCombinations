369521878a02cbfdbbe3ba6e9a42b9a71d2413a3
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-19 16:28:28 -0700

    Initialize new branch and alternative testing with no instance of Coins class
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Richard Barbour <richyb07@gmail.com>

--- CoinCombinations.Tests/ModelTests/CoinCombinationsTests.cs
+++ CoinCombinations.Tests/ModelTests/CoinCombinationsTests.cs
@@ -8,33 +8,13 @@ namespace CoinCombinations.Tests
   [TestClass]
   public class CoinsTests
   {
-  [TestMethod]
-  public void CalcChange_GetValueOfChange_Int()
-  {
-    int userInput = 67;
-    Coins coins = new Coins(userInput);
-    int value = coins.Change;
-    Assert.AreEqual(userInput, value);
-  }
-
-  [TestMethod]
-  public void CalcChange_SetsValueOfChange_Int()
-  {
-    Coins coins = new Coins(67);
-    int newChangeNumber = 34;
-    coins.Change = newChangeNumber;
-    Assert.AreEqual(newChangeNumber, coins.Change);
-
-  }
-
-  [TestMethod]
-  public void CalcChange_CreatesListOfChangeType_Void()
-  {
-    Coins coins = new Coins(67);
-    coins.CalcChange();
+[TestMethod]
+    public void CalcChange_CreatesListOfChangeType_Void()
+    {
+    Coins.CalcChange(67);
     List<int> expected = new List<int> { 2, 1, 1, 2 };
-    CollectionAssert.AreEqual(expected, coins._results);
-  }
+    CollectionAssert.AreEqual(expected, Coins._results);
+    }
   }
 }
 
--- CoinCombinations/Models/CoinCombinations.cs
+++ CoinCombinations/Models/CoinCombinations.cs
@@ -7,17 +7,13 @@ namespace CoinCombinations.Models
   {
     public static int[] coinsArray = new int[] { 25, 10, 5, 1 };
 
-    public List<int> _results = new List<int> {}; 
+    public static List<int> _results = new List<int> {}; 
 
-    public int Change { get; set; }
-
-    public Coins(int change)
+    public static int Change { get; set; }
+    public static void CalcChange(int change)
     {
       Change = change;
-    }
-
-    public void CalcChange()
-    {
+      
       int removedChange;
       int result;
 
